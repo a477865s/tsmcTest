9a7aea2856937b60125ac177d429e28783a82121
a477865s <a477865s@gmail.com>
2021-05-22 08:57:07 +0800

    FirstCommit

---
+++ tsmcTest/tsmcTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tsmcTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
